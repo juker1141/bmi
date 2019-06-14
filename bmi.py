e3b940f639fb72b1da09bf56b00632529ec9ca03
juker1141 <50107434+juker1141@users.noreply.github.com>
2019-06-14 14:24:38 +0800

    Update bmi.py

--- bmi.py
+++ bmi.py
@@ -1,21 +1,30 @@
-hi = input('請輸入身高: ')
-wei = input('請輸入體重: ')
-him = float(float(hi) / 100)
-wei = float(wei)
-bmi = wei / (him * him)
-bmi = float(bmi)
+def bmi1():
+	high = float(input('請輸入身高(公分): '))
+	weight = float(input('請輸入體重: '))
+	high = high / 100
+	bmi = weight / (high * high)
+	bmi = str(round(bmi, 2))
+	return bmi
 
-print('BMI=' ,bmi)
+def judge1(bmi):
+	
+	if bmi < 18.5:
+		judge = '過輕'
+	elif bmi >= 18.5 and bmi < 24:
+		judge = '正常'
+	elif bmi >= 24 and bmi < 27:
+		judge = '過重'
+	elif bmi >= 27 and bmi <30:
+		judge = '輕度肥胖'
+	elif bmi >= 30 and bmi <35:
+		judge = '中度肥胖'
+	elif bmi >= 35:
+		judge = '重度肥胖'
+	return judge
 
-if bmi < 18.5:
-    print('過輕')
-elif bmi >= 18.5 and bmi < 24:
-    print('適中')
-elif bmi >= 24 and bmi < 27:
-    print('過重')
-elif bmi >= 27 and bmi < 30:
-    print('輕度肥胖')
-elif bmi >= 30 and bmi <35:
-    print('中度肥胖')
-else :
-	print('重度肥胖')
+def main():
+	bmi = bmi1()
+	judge = judge1(float(bmi))
+	print('您的bmi指數為' + bmi + ', ' + judge)
+
+main()
